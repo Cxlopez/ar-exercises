631fde927bd79e1437adcba08fe0378c53344b6f
Cxlopez <cristian4402@hotmail.com>
2022-09-12 18:44:58 -0400

    calculate annual avg, total, $ > 1000000

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,6 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+puts "The total revenue of all stores is #{Store.sum(:annual_revenue)}"
+puts "The average annual revenue for all stores is #{Store.average(:annual_revenue)}"
+puts "The number of stores with more than $1m in annual sales is #{Store.where("annual_revenue >= 1000000").count}"
