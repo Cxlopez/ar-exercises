6494d8cea63e136ddbce4d38078df76800059656
Cxlopez <cristian4402@hotmail.com>
2022-09-12 16:59:12 -0400

    Create 3 stores, update first store name to gucci

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,9 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+puts "@store1 is #{@store1.name}"
+@store1.name = 'Gucci'
+puts "@store1 is now called #{@store1.name}"
+@store2 = Store.find_by(id: 2)
+puts "@store2 is #{@store2.name}"
