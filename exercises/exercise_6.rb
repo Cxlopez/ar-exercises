a54c512b411620ca95103774c9569912801d7c79
Cxlopez <cristian4402@hotmail.com>
2022-09-12 19:01:17 -0400

    add employee

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Cristian", last_name: "Sanchez", hourly_rate: 95)
+@store1.employees.create(first_name: "Mister", last_name: "One", hourly_rate: 1)
+@store1.employees.create(first_name: "Miss", last_name: "Sunday", hourly_rate: 70)
+@store2.employees.create(first_name: "Miss", last_name: "Wednesday", hourly_rate: 90)
+@store2.employees.create(first_name: "Mister", last_name: "Three", hourly_rate: 35)
